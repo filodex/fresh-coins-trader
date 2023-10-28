82343f65b535e4899cdd0228692b3249f4dac532
filodex <36601598+filodex@users.noreply.github.com>
2023-10-28 11:42:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 import 'dotenv/config'
-import etherscanApi from './src/Apis/EtherscanApi/EtherscanApi.js'
+import etherscanApi from './src/apis/EtherscanApi/EtherscanApi.js'
+import configService from './src/services/config.service.js'
+import whalesDetectorService from './src/Services/WhalesDetector.service.js'
+
+const titanxTokenAddress = '0xc45a81bc23a64ea556ab4cdf08a86b61cdceea8b'
+
+const { setOfWalletsBoughtThisToken: array } = await whalesDetectorService.getSetOfWalletsBoughtThisToken({
+    address: titanxTokenAddress,
+})
+console.log('setOfWallets', array)
+await whalesDetectorService.findGoodWhalesInListOfWallets({ setOfWallets: array })
 
 setInterval(() => {}, 100000)
