671cf38416b1392a283386eef361f941444615ee
filodex <36601598+filodex@users.noreply.github.com>
2023-10-28 09:24:30 +0300

    Create WhalesDetector.service.js

---
+++ src/services/WhalesDetector.service.js
@@ -0,0 +1,6 @@
+import etherscanApi from './src/apis/EtherscanApi/EtherscanApi.js'
+
+export class WhalesDetectorService {
+
+const whalesDetectorService = new WhalesDetectorService()
+export default whalesDetectorService
