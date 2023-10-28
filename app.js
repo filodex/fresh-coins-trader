e99d4831b9d018edaaac13334d96687952c06bad
filodex <36601598+filodex@users.noreply.github.com>
2023-10-28 09:23:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 import 'dotenv/config'
 import etherscanApi from './src/Apis/EtherscanApi/EtherscanApi.js'
 
-console.log(
-    await etherscanApi.getListOfTokenTransfers({ address: '0x1f9090aaE28b8a3dCeaDf281B0F12828e676c326' })
-)
-
 setInterval(() => {}, 100000)
