6c3607cca77ee4f20e8ecbb73250d5aeeb37059c
filodex <36601598+filodex@users.noreply.github.com>
2023-10-28 09:25:34 +0300

    Update EtherscanApi.js

--- src/Apis/EtherscanApi/EtherscanApi.js
+++ src/Apis/EtherscanApi/EtherscanApi.js
@@ -3,6 +3,26 @@ export class EtherscanApi {
     constructor(apiToken) {
         this.#apiToken = apiToken
     }
+    async getWalletBalance({ address }) {
+        try {
+            const url = new URL('https://api.etherscan.io/api?module=account&action=tokentx')
+            url.searchParams.append('apikey', this.#apiToken)
+            url.searchParams.append('offset', offset)
+            url.searchParams.append('page', page)
+            url.searchParams.append('sort', sort)
+            address ? url.searchParams.append('address', address) : null
+
+            const res = await fetch(url)
+            const resJson = await res.json()
+            if (resJson.message !== 'OK') {
+                console.log(resJson)
+                throw new Error(`resJson message: ${resJson.message}, resJson result: ${resJson.result}`)
+            }
+            return {}
+        } catch (error) {
+            throw new Error('Error in getListOfTokenTransfers' + error)
+        }
+    }
     async getListOfTokenTransfers({ address, offset = 50, page = 1, sort = 'desc' }) {
         try {
             const url = new URL('https://api.etherscan.io/api?module=account&action=tokentx')
@@ -18,7 +38,7 @@ export class EtherscanApi {
                 console.log(resJson)
                 throw new Error(`resJson message: ${resJson.message}, resJson result: ${resJson.result}`)
             }
-            return resJson.result
+            return { listOfTokenTransfers: resJson.result }
         } catch (error) {
             throw new Error('Error in getListOfTokenTransfers' + error)
         }
