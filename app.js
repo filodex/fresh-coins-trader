3a800523a5a4c2460a804bbeb64a4b0b45364af3
filodex <36601598+filodex@users.noreply.github.com>
2023-10-28 17:44:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ import 'dotenv/config'
 import etherscanApi from './src/apis/EtherscanApi/EtherscanApi.js'
 import configService from './src/services/config.service.js'
 import whalesDetectorService from './src/Services/WhalesDetector.service.js'
+import syveApi from './src/apis/SyveApi.js'
 
 const titanxTokenAddress = '0xc45a81bc23a64ea556ab4cdf08a86b61cdceea8b'
+const walletsToCheckProfit = [
+    '0x4a7c6899cdcb379e284fbfd045462e751da4c7ce',
+    '0xdb5889e35e379ef0498aae126fc2cce1fbd23216',
+    '0x00000047bb99ea4d791bb749d970de71ee0b1a34',
+    '0xdef1c0ded9bec7f1a1670819833240f027b25eff',
+]
 
-const { setOfWalletsBoughtThisToken: array } = await whalesDetectorService.getSetOfWalletsBoughtThisToken({
+const { addressesWithGoodProfit } = await whalesDetectorService.getProfitableWhaleWalletsBoughtThisToken({
     address: titanxTokenAddress,
 })
-console.log('setOfWallets', array)
-await whalesDetectorService.findGoodWhalesInListOfWallets({ setOfWallets: array })
+console.log(addressesWithGoodProfit)
 
 setInterval(() => {}, 100000)
