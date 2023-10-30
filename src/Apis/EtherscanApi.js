be3aec8f525a84bda67ba7c4cd2ebda7a395bc6b
filodex <filodex@gmail.com>
2023-10-30 21:03:51 +0300

    Create EtherscanApi.js

---
+++ src/Apis/EtherscanApi.js
@@ -0,0 +1,68 @@
+export class EtherscanApi {
+    #apiToken
+    constructor(apiToken) {
+        this.#apiToken = apiToken
+    }
+    async getWalletTokenBalanceForSingleAddressByContractAddress({ address, contractAddress }) {
+        try {
+            const url = new URL('https://api.etherscan.io/api?module=account&action=tokenbalance&address')
+            url.searchParams.append('apikey', this.#apiToken)
+            url.searchParams.append('address', address)
+            url.searchParams.append('contractaddress', contractAddress ?? '')
+            console.log(url.toString())
+
+            const res = await fetch(url)
+            const resJson = await res.json()
+
+            if (resJson.message !== 'OK') {
+                console.log('Something wrong in getWalletBalanceForSingleAddress', resJson)
+                throw new Error(`resJson message: ${resJson.message}, resJson result: ${resJson.result}`)
+            }
+            return { tokenBalance: resJson.result }
+        } catch (error) {
+            throw new Error('Error in getWalletTokenBalanceForSingleAddress' + error)
+        }
+    }
+    async getWalletEtherBalanceForSingleAddress({ address }) {
+        try {
+            const url = new URL('https://api.etherscan.io/api?module=account&action=balance')
+            url.searchParams.append('apikey', this.#apiToken)
+            url.searchParams.append('address', address)
+
+            const res = await fetch(url)
+            const resJson = await res.json()
+
+            if (resJson.message !== 'OK') {
+                console.log('Something wrong in getWalletBalanceForSingleAddress', resJson)
+                throw new Error(`resJson message: ${resJson.message}, resJson result: ${resJson.result}`)
+            }
+
+            return { etherBalance: resJson.result / 10 ** 18, weiBalance: resJson.result }
+        } catch (error) {
+            throw new Error('Error in getListOfTokenTransfers' + error)
+        }
+    }
+    async getListOfTokenTransfers({ address, offset = 600, page = 1, sort = 'desc' }) {
+        try {
+            const url = new URL('https://api.etherscan.io/api?module=account&action=tokentx')
+            url.searchParams.append('apikey', this.#apiToken)
+            url.searchParams.append('offset', offset)
+            url.searchParams.append('page', page)
+            url.searchParams.append('sort', sort)
+            address ? url.searchParams.append('address', address) : null
+
+            const res = await fetch(url)
+            const resJson = await res.json()
+            if (resJson.message !== 'OK') {
+                console.log('Something wrong in getListOfTokenTransfers', resJson)
+                throw new Error(`resJson message: ${resJson.message}, resJson result: ${resJson.result}`)
+            }
+            return { listOfTokenTransfers: resJson.result }
+        } catch (error) {
+            throw new Error('Error in getListOfTokenTransfers' + error)
+        }
+    }
+}
+
+const etherscanApi = new EtherscanApi(process.env.ETHERSCAN_TOKEN)
+export default etherscanApi
