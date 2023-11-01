8843df05dc0d9345b66dbd866a223f048ad6c7a5
filodex <36601598+filodex@users.noreply.github.com>
2023-11-01 17:47:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,16 +4,13 @@ import configService from './src/services/Config.service.js'
 import whalesDetectorService from './src/Services/WhalesDetector.service.js'
 import syveApi from './src/apis/SyveApi.js'
 import EthAddress from './src/model/EthAddress.js'
+import traderService from './src/services/Trader.service.js'
 
-const titanxTokenAddress = '0xc45a81bc23a64ea556ab4cdf08a86b61cdceea8b'
-const walletsToCheckProfit = [
-    '0x4a7c6899cdcb379e284fbfd045462e751da4c7ce',
-    '0xdb5889e35e379ef0498aae126fc2cce1fbd23216',
-    '0x00000047bb99ea4d791bb749d970de71ee0b1a34',
-    '0xdef1c0ded9bec7f1a1670819833240f027b25eff',
-]
-
-const { listOfWallets } = whalesDetectorService.readListOfWhaleWalletsFromFile()
-console.log(listOfWallets)
+/**
+ * Нашел токены, которые купили несколько крутых ребяt
+ * Узнать цену, по которой я могу сейчас тоже купить
+ * Запомнить цену
+ */
+console.log(await traderService.findTokensTradedByGoodWhales())
 
 setInterval(() => {}, 100000)
