3867fc2b8d56c3d376ce1934678d8f2af5bcb4cd
filodex <36601598+filodex@users.noreply.github.com>
2023-11-05 15:57:12 +0300

    Create DexScreenerApi.ts

---
+++ src/apis/DexScreenerApi.ts
@@ -0,0 +1,20 @@
+import { Pair } from './IDexScreenerApi'
+
+export class DexScreenerApi {
+    async getPairsDataByName(name: string): Promise<Pair[]> {
+        try {
+            const url = new URL('https://api.dexscreener.com/latest/dex/search/')
+            url.searchParams.append('q', name + '/WETH')
+
+            const res = await fetch(url)
+            const resJson = await res.json()
+
+            return resJson.pairs
+        } catch (error) {
+            throw new Error('Error in getPairDataByName' + error)
+        }
+    }
+}
+
+const dexScreenerApi = new DexScreenerApi()
+export default dexScreenerApi
