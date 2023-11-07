67db8752ad2c910a4e8a2a5b5bc7eaab85edcb8f
filodex <filodex@gmail.com>
2023-11-07 23:19:48 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,10 @@ import etherscanApi from './apis/EtherscanApi.js'
 // import whalesDetectorService from './services/WhalesDetector.service.js'
 import { sleep } from './utils/utils.js'
 import path from 'path'
-import traderService from './services/Trader.service.js'
+// import traderService from './services/Trader.service.js'
 import fs from 'fs'
-import dexScreenerApi from './apis/DexScreenerApi.js'
-import telegramBotService from './services/TelegramBot.service.js'
+// import dexScreenerApi from './apis/DexScreenerApi.js'
+// import telegramBotService from './services/TelegramBot.service.js'
 
 process.on('uncaughtException', async (err) => {
     console.log(err)
@@ -22,68 +22,72 @@ const tokensBoughtSet = new Set()
 /**
  * MAIN
  */
-await writeGoodTrades()
+// await writeGoodTrades()
 // await telegramBotService.handleBotCommands()
-// await telegramBotService.sendMessageToMyChannel('hehe hoho')
+
+const transfers = await etherscanApi.getListOfTokenTransfers({
+    address: '0x3f744aaFD19d6f8Db011FB2Acc67375cb2B5189F',
+})
+console.log(transfers)
 
 setInterval(() => {}, 5000)
 
-async function writeGoodTrades() {
-    try {
-        traderService.updateListOfEthAddressesFromFile()
+// async function writeGoodTrades() {
+//     try {
+//         traderService.updateListOfEthAddressesFromFile()
 
-        const { tokensTradedByMoreThanOneWallet } = await traderService.findTokensTradedByGoodWhales()
-        console.log('tokensTradedByMoreThanOneWallet', tokensTradedByMoreThanOneWallet)
-        for (const key in tokensTradedByMoreThanOneWallet) {
-            if (tokensTradedByMoreThanOneWallet[key].walletsCount >= 2) {
-                if (tokensBoughtSet.has(key)) {
-                    continue
-                }
+//         const { tokensTradedByMoreThanOneWallet } = await traderService.findTokensTradedByGoodWhales()
+//         console.log('tokensTradedByMoreThanOneWallet', tokensTradedByMoreThanOneWallet)
+//         for (const key in tokensTradedByMoreThanOneWallet) {
+//             if (tokensTradedByMoreThanOneWallet[key].walletsCount >= 2) {
+//                 if (tokensBoughtSet.has(key)) {
+//                     continue
+//                 }
 
-                let tokenPrice
-                try {
-                    const pairs = await dexScreenerApi.getPairsDataByName(
-                        tokensTradedByMoreThanOneWallet[key]?.tokenSymbol
-                    )
+//                 let tokenPrice
+//                 try {
+//                     const pairs = await dexScreenerApi.getPairsDataByName(
+//                         tokensTradedByMoreThanOneWallet[key]?.tokenSymbol
+//                     )
 
-                    for (const pair of pairs) {
-                        if (pair.pairCreatedAt || 0 > new Date().getTime() - 1000 * 60 * 60 * 24) {
-                            tokenPrice = pair.priceUsd
-                            break
-                        }
-                    }
-                } catch (error) {
-                    console.log(error)
-                }
+//                     for (const pair of pairs) {
+//                         if (pair.pairCreatedAt || 0 > new Date().getTime() - 1000 * 60 * 60 * 24) {
+//                             tokenPrice = pair.priceUsd
+//                             break
+//                         }
+//                     }
+//                 } catch (error) {
+//                     console.log(error)
+//                 }
 
-                try {
-                    telegramBotService.sendMessageToMyChannel(
-                        `Вот хэш токена ${tokensTradedByMoreThanOneWallet[key]?.tokenName}, покупай\n${key}\nЕго купили ${tokensTradedByMoreThanOneWallet[key]?.walletsCount} кит(ов)`
-                    )
-                } catch (error) {
-                    console.log(error)
-                }
+//                 try {
+//                     telegramBotService.sendMessageToMyChannel(
+//                         `Вот хэш токена ${tokensTradedByMoreThanOneWallet[key]?.tokenName}, покупай\n${key}\nЕго купили ${tokensTradedByMoreThanOneWallet[key]?.walletsCount} кит(ов)`
+//                     )
+//                 } catch (error) {
+//                     console.log(error)
+//                 }
 
-                tokensBoughtSet.add(key)
-                const jsonToWrite = JSON.stringify({
-                    tokenAddress: key,
-                    boughtAt: new Date(),
-                    walletsCount: tokensTradedByMoreThanOneWallet[key]?.walletsCount,
-                    lastBuy: tokensTradedByMoreThanOneWallet[key]?.lastBuyDate,
-                    tokenName: tokensTradedByMoreThanOneWallet[key]?.tokenName,
-                    price: tokenPrice,
-                })
-                // console.log('jsonToWrite', jsonToWrite)
-                fs.appendFileSync(path.join(path.resolve(), 'diff', 'tokensBought.txt'), '\n' + jsonToWrite)
-            }
-        }
-        setTimeout(() => {
-            writeGoodTrades()
-        }, 20000)
-    } catch (error) {
-        console.log(error)
-        setTimeout(() => {
-            writeGoodTrades()
-        }, 20000)
-    }
-}
+//                 tokensBoughtSet.add(key)
+//                 const jsonToWrite = JSON.stringify({
+//                     tokenAddress: key,
+//                     boughtAt: new Date(),
+//                     walletsCount: tokensTradedByMoreThanOneWallet[key]?.walletsCount,
+//                     lastBuy: tokensTradedByMoreThanOneWallet[key]?.lastBuyDate,
+//                     tokenName: tokensTradedByMoreThanOneWallet[key]?.tokenName,
+//                     price: tokenPrice,
+//                 })
+//                 // console.log('jsonToWrite', jsonToWrite)
+//                 fs.appendFileSync(path.join(path.resolve(), 'diff', 'tokensBought.txt'), '\n' + jsonToWrite)
+//             }
+//         }
+//         setTimeout(() => {
+//             writeGoodTrades()
+//         }, 20000)
+//     } catch (error) {
+//         console.log(error)
+//         setTimeout(() => {
+//             writeGoodTrades()
+//         }, 20000)
+//     }
+// }
