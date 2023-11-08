dcd34abc0a46fb832cf912701663622fc8648cad
filodex <filodex@gmail.com>
2023-11-08 23:44:16 +0300

    Update DexScreenerApi.ts

--- src/apis/DexScreenerApi.ts
+++ src/apis/DexScreenerApi.ts
@@ -14,6 +14,25 @@ export class DexScreenerApi {
             throw new Error('Error in getPairDataByName' + error)
         }
     }
+
+    async getPairsDataByTokenAddress({ tokenAddress }: { tokenAddress: string }): Promise<Pair[]> {
+        try {
+            const url = new URL(`https://api.dexscreener.com/latest/dex/tokens/${tokenAddress}`)
+
+            const res = await fetch(url)
+            const resJson = await res.json()
+
+            console.log('resJson in getPairsDataByTokenAddress', resJson)
+
+            if (!Array.isArray(resJson.pairs)) {
+                throw new Error('No pairs got from getPairsDataByTokenAddress')
+            }
+
+            return resJson.pairs
+        } catch (error) {
+            throw new Error('Error in getPairsDataByTokenAddress' + error)
+        }
+    }
 }
 
 const dexScreenerApi = new DexScreenerApi()
