1534ee302c34fcec6468953469565ef8a961c5bd
filodex <36601598+filodex@users.noreply.github.com>
2023-11-10 15:08:46 +0300

    Update EthAddress.ts

--- src/model/EthAddress.ts
+++ src/model/EthAddress.ts
@@ -3,10 +3,16 @@ import fs from 'fs'
 
 export default class EthAddress {
     address: string
-    latestTokenTransfers: any
-    totalWalletPerformance: any
+    latestTokenTransfers?: any
+    totalWalletPerformance?: any
 
-    constructor({ address, totalWalletPerformance }: { address: string; totalWalletPerformance?: any }) {
+    constructor({
+        address,
+        totalWalletPerformance,
+    }: {
+        address: string
+        totalWalletPerformance?: any
+    }) {
         this.address = address
         this.totalWalletPerformance = totalWalletPerformance
     }
@@ -14,8 +20,14 @@ export default class EthAddress {
     async updateBalance() {}
 
     async appendToListInFile() {
-        const goodWhalesJsonPath = path.join(path.resolve(), 'diff', 'goodWhales.json')
-        const fileText = fs.readFileSync(goodWhalesJsonPath, { encoding: 'utf-8' })
+        const goodWhalesJsonPath = path.join(
+            path.resolve(),
+            'diff',
+            'goodWhales.json'
+        )
+        const fileText = fs.readFileSync(goodWhalesJsonPath, {
+            encoding: 'utf-8',
+        })
         const jsoned = JSON.parse(fileText)
         jsoned.push(this)
         fs.writeFileSync(goodWhalesJsonPath, JSON.stringify(jsoned, null, 2))
